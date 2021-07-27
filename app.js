edeb68d88cb0431709b8bb6ae5fe9a375ca5b8a3
Dragon <bhadurmister@gmail.com>
2021-07-27 16:57:30 +0530

    m

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let sign = document.getElementById("sign")
 var signIn = document.getElementsByClassName("sign-in")
 var headerInside = document.getElementsByClassName("header__inside")
+let tabBtn = document.getElementsById("tab1");
+let tabPara = document.getElementsByClassName("tab");
+tabBtn.addEventListener('click',()=>{
+    console.log('hell');
+})
 sign.addEventListener('click', () => {
     for (let i = 0; i < signIn.length; i++) {
         signIn[i].style.display = 'block'
@@ -9,7 +14,7 @@ sign.addEventListener('click', () => {
         headerInside[i].style.display = 'none'
     }
 })
-sign.addEventListener('dblclick' , ()=>{
+sign.addEventListener('dblclick', () => {
     for (let i = 0; i < signIn.length; i++) {
         signIn[i].style.display = 'none'
     }
@@ -17,5 +22,11 @@ sign.addEventListener('dblclick' , ()=>{
         headerInside[i].style.display = 'block'
     }
 })
+
+
+
+
+
+
 
 
